a6ef74153a14d4387bd30e4edb3d67653c86b967
sharqX <shariquerahman24@outlook.com>
2024-09-02 14:33:16 +0530

    prints triangles

--- myapp/hello.py
+++ myapp/hello.py
@@ -1,7 +1,7 @@
-import fire
+def main(a):
+    for i in range(a):
+      for j in range(i+1):
+        print("* ", end="")
+      print()
 
-def hello(name="Sharique"):
-  return "Hello %s!" % name
-
-if __name__ == '__main__':
-  fire.Fire(hello)
+main(16)
